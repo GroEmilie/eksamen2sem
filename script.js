1e7a82e53bbcb39660233e5c27718f6ded75d338
Jeeanett <jeanett5@hotmail.com>
2022-05-30 20:53:05 +0200

    hej

--- script.js
+++ script.js
@@ -29,36 +29,3 @@ function showSlides(n) {
   dots[slideIndex -1].className += " active";
 }
 
-
-
-
-// console.log ('Velkommen til loggen');
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-
-// // Næste/forrige controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("prik");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex -1].className += " active";
-// }
